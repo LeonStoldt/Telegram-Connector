3562d40231667836cd596c94421da481bc7b4ca8
Leon Stoldt <leon.stoldt.a17b@nordakademie.org>
2019-12-08 14:16:22 +0100

    splitted sql scripts to initialize postgres with data on docker compose startup

---
+++ src/main/resources/database/2-data.sql
@@ -0,0 +1,10 @@
+CREATE TABLE telegram.clients
+(
+    chatId bigint NOT NULL PRIMARY KEY,
+    active boolean NOT NULL,
+    firstName text,
+    lastName text,
+    userName text NOT NULL,
+    title text,
+    type text NOT NULL
+)
