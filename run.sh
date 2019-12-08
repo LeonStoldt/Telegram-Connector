4dc91c3bca3fef599960cffbf3ef916c49606743
Tim Rader <tim.rader@otto.de>
2019-12-09 00:43:31 +0100

    Optimize shell run script

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 WORKINGDIR=$(pwd)
 
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
+
 git clone https://gitlab2.nordakademie.de/cloud-fundamentals/telegram-connector.git
 git clone https://gitlab2.nordakademie.de/cloud-fundamentals/distributionservice.git
 git clone https://gitlab2.nordakademie.de/cloud-fundamentals/nordbahnservice.git
@@ -9,22 +12,23 @@ git clone https://gitlab2.nordakademie.de/cloud-fundamentals/translateservice.gi
 git clone https://gitlab2.nordakademie.de/cloud-fundamentals/urlshortenerservice.git
 git clone https://gitlab2.nordakademie.de/cloud-fundamentals/wikipediaservice.git
 
-cd $WORKINGDIR/telegram-connector && git pull
-cd $WORKINGDIR/distributionservice && git pull
-cd $WORKINGDIR/nordbahnservice && git pull
-cd $WORKINGDIR/weatherservice && git pull
-cd $WORKINGDIR/translateservice && git pull
-cd $WORKINGDIR/urlshortenerservice && git pull
-cd $WORKINGDIR/wikipediaservice git pull
+cd $WORKINGDIR/telegram-connector && git pull && \
+cd $WORKINGDIR/distributionservice && git pull && \
+cd $WORKINGDIR/nordbahnservice && git pull && \
+cd $WORKINGDIR/weatherservice && git pull && \
+cd $WORKINGDIR/translateservice && git pull && \
+cd $WORKINGDIR/urlshortenerservice && git pull  && \
+cd $WORKINGDIR/wikipediaservice git pull && \
 
-mvn -f $WORKINGDIR/telegram-connector/pom.xml -Dmaven.test.skip=true package
-mvn -f $WORKINGDIR/distributionservice/pom.xml -Dmaven.test.skip=true package
-mvn -f $WORKINGDIR/nordbahnservice/pom.xml -Dmaven.test.skip=true package
-mvn -f $WORKINGDIR/weatherservice/pom.xml -Dmaven.test.skip=true package
-mvn -f $WORKINGDIR/translateservice/pom.xml -Dmaven.test.skip=true package
-mvn -f $WORKINGDIR/urlshortenerservice/pom.xml -Dmaven.test.skip=true package
+mvn -f $WORKINGDIR/telegram-connector/pom.xml -Dmaven.test.skip=true package && \
+mvn -f $WORKINGDIR/distributionservice/pom.xml -Dmaven.test.skip=true package && \
+mvn -f $WORKINGDIR/nordbahnservice/pom.xml -Dmaven.test.skip=true package && \
+mvn -f $WORKINGDIR/weatherservice/pom.xml -Dmaven.test.skip=true package && \
+mvn -f $WORKINGDIR/translateservice/pom.xml -Dmaven.test.skip=true package && \
+mvn -f $WORKINGDIR/urlshortenerservice/pom.xml -Dmaven.test.skip=true package && \
 
-cp $WORKINGDIR/telegram-connector/docker-compose.yaml $WORKINGDIR/docker-compose.yaml
+cp $WORKINGDIR/telegram-connector/docker-compose.yaml $WORKINGDIR/docker-compose.yaml && \
 
 cd $WORKINGDIR
-docker-compose up --build
+docker-compose down && \
+docker-compose up --build --force-recreate
