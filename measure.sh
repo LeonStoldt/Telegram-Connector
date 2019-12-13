d08613175405ec4fdf964d9f58d777852610a14e
Leon Stoldt <leon.stoldt.a17b@nordakademie.org>
2019-12-13 12:02:10 +0100

    fixed measure statement to use siege as load test and deleted Documentation.md in view of parallel existing and more recent word document

--- measure.sh
+++ measure.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-echo How often do you want to curl?
-read TIMES
-
-for i in {1..$TIMES}
-do
- 
-  curl -H 'Content-Type: application/json' localhost:8079/api -d '{"chatId":1337,"message":"/info","firstName":"Test","lastName":"User","userName":null,"title":null,"chatType":"Private"}' -s -o /dev/null -w '%{time_starttransfer}\n' "$@"
-
-done
+siege -t 120S --content-type "application/json" 'http://localhost:4003/api POST {"chatId":1337,"message":"/hilfe","firstName":"Test","lastName":"User","userName":null,"title":null,"chatType":"Private"}'
