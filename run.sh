dee647ec2d4b5fdc89fcdd7c1630476753c9eb67
LeonStoldt <leon-stoldt@web.de>
2020-01-05 18:50:08 +0100

    updated run.sh

--- run.sh
+++ run.sh
@@ -5,31 +5,31 @@ WORKINGDIR=$(pwd)
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=3600'
 
-git clone https://gitlab2.nordakademie.de/cloud-fundamentals/telegram-connector.git
-git clone https://gitlab2.nordakademie.de/cloud-fundamentals/distributionservice.git
-git clone https://gitlab2.nordakademie.de/cloud-fundamentals/nordbahnservice.git
-git clone https://gitlab2.nordakademie.de/cloud-fundamentals/weatherservice.git
-git clone https://gitlab2.nordakademie.de/cloud-fundamentals/translateservice.git
-git clone https://gitlab2.nordakademie.de/cloud-fundamentals/urlshortenerservice.git
-git clone https://gitlab2.nordakademie.de/cloud-fundamentals/wikipediaservice.git
+git clone https://github.com/LeonStoldt/Telegram-Connector.git
+git clone https://github.com/LeonStoldt/Distribution-Service.git
+git clone https://github.com/LeonStoldt/Nordbahn-Service.git
+git clone https://github.com/LeonStoldt/Translate-Service.git
+git clone https://github.com/LeonStoldt/URL-Shortener-Service.git
+git clone https://github.com/LeonStoldt/Weather-Service.git
+git clone https://github.com/LeonStoldt/Wikipedia-Service.git
 
-cd $WORKINGDIR/telegram-connector && git pull && \
-cd $WORKINGDIR/distributionservice && git pull && \
-cd $WORKINGDIR/nordbahnservice && git pull && \
-cd $WORKINGDIR/weatherservice && git pull && \
-cd $WORKINGDIR/translateservice && git pull && \
-cd $WORKINGDIR/urlshortenerservice && git pull  && \
-cd $WORKINGDIR/wikipediaservice git pull && \
+cd $WORKINGDIR/Telegram-Connector && git pull && \
+cd $WORKINGDIR/Distribution-Service && git pull && \
+cd $WORKINGDIR/Nordbahn-Service && git pull && \
+cd $WORKINGDIR/WeatherS-ervice && git pull && \
+cd $WORKINGDIR/Translate-Service && git pull && \
+cd $WORKINGDIR/URL-Shortener-Service && git pull  && \
+cd $WORKINGDIR/Wikipedia-Service git pull && \
 
-mvn -f $WORKINGDIR/telegram-connector/pom.xml -Dmaven.test.skip=true package && \
-mvn -f $WORKINGDIR/distributionservice/pom.xml -Dmaven.test.skip=true package && \
-mvn -f $WORKINGDIR/nordbahnservice/pom.xml -Dmaven.test.skip=true package && \
-mvn -f $WORKINGDIR/weatherservice/pom.xml -Dmaven.test.skip=true package && \
-mvn -f $WORKINGDIR/translateservice/pom.xml -Dmaven.test.skip=true package && \
-mvn -f $WORKINGDIR/urlshortenerservice/pom.xml -Dmaven.test.skip=true package && \
+mvn -f $WORKINGDIR/Telegram-Connector/pom.xml -Dmaven.test.skip=true package && \
+mvn -f $WORKINGDIR/Distribution-Service/pom.xml -Dmaven.test.skip=true package && \
+mvn -f $WORKINGDIR/Nordbahn-Service/pom.xml -Dmaven.test.skip=true package && \
+mvn -f $WORKINGDIR/Weather-Service/pom.xml -Dmaven.test.skip=true package && \
+mvn -f $WORKINGDIR/Translate-Service/pom.xml -Dmaven.test.skip=true package && \
+mvn -f $WORKINGDIR/URL-Shortener-Service/pom.xml -Dmaven.test.skip=true package && \
 
-cp $WORKINGDIR/telegram-connector/docker-compose.yaml $WORKINGDIR/docker-compose.yaml && \
-cp $WORKINGDIR/telegram-connector/haproxy.cfg $WORKINGDIR/haproxy.cfg && \
+cp $WORKINGDIR/Telegram-Connector/docker-compose.yaml $WORKINGDIR/docker-compose.yaml && \
+cp $WORKINGDIR/Telegram-Connector/haproxy.cfg $WORKINGDIR/haproxy.cfg && \
 
 cd $WORKINGDIR
 docker-compose down && \
